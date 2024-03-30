f67cab07bb66e38913cf7f08288c587251fb6012
najlabahtiar <165452001+najlabahtiar@users.noreply.github.com>
2024-03-30 11:33:26 +0700

    upload 1

---
+++ test.py
@@ -0,0 +1,15 @@
+import streamlit as st
+
+st.write('test streamlit')
+st.write('najla bahtiar')
+st. title('wooooiii')
+st.header('This is a header with a divider', divider='rainbow')
+
+angka_pertama = st.number_input('masukkan angka pertama')
+st.write('the first number is ', angka_pertama)
+
+angka_kedua = st.number_input('masukkan angka kedua')
+st.write('the second number is ', angka_kedua)
+
+operasi_matematika = angka_pertama * angka_kedua
+st.write    (f"angka pertama {angka_pertama} x angka kedua {angka_kedua} = {operasi_matematika}")
